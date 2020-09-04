9c0f5291d34714fd017f31e26c483168d83ff9c6
ACHU <achusp123@gmail.com>
2020-09-04 11:24:42 +0200

    Update contact page with latest methods

--- src/pageObjects/ContactPage.java
+++ src/pageObjects/ContactPage.java
@@ -17,15 +17,17 @@ public class ContactPage extends MasterPage {
 
 		return this;
 	}
-
-	public static LocatorObject Contact_Subject_Dropdown = new LocatorObject("//div[@class='selector']/select","Contact subject", LocatorObject.XPATH);
-	public static LocatorObject Contact_Email_Textbox = new LocatorObject("//input[@id='email']","Contact email", LocatorObject.XPATH);
-	public static LocatorObject Contact_Order_Textbox = new LocatorObject("//input[@id='id_order']","Contact order number", LocatorObject.XPATH);
-	public static LocatorObject Contact_Message_TextArea = new LocatorObject("//textarea[@id='message']", "Contact Message",LocatorObject.XPATH);
-	public static LocatorObject Contact_Send_Button = new LocatorObject("//button[@id='submitMessage']","Contact send button", LocatorObject.XPATH);
-	public static LocatorObject Contact_Error_Label = new LocatorObject("//div[@class='alert alert-danger']/ol/li", "Contact error message", LocatorObject.XPATH);
-	public static LocatorObject Contact_Success_Label = new LocatorObject("//p[@class='alert alert-success']", "Contact success message", LocatorObject.XPATH);
+	int i=0;
 	
+	public static LocatorObject Contact_Firstname_Textbox = new LocatorObject("//input[@id='first-name']","First name", LocatorObject.XPATH);
+	public static LocatorObject Contact_Lastname_Textbox = new LocatorObject("//input[@id='last-name']","Last name", LocatorObject.XPATH);
+	public static LocatorObject Contact_Jobtitle_Textbox = new LocatorObject("//input[@id='job-title']","Job title", LocatorObject.XPATH);
+	public static LocatorObject Contact_Yearofexp_Dropdown = new LocatorObject("//div[@class='form-group']/div[6]/select","Year of experience", LocatorObject.XPATH);
+	
+	public static LocatorObject Contact_Date_Textbox = new LocatorObject("//div[@class='form-group']/div[7]/input","Date", LocatorObject.XPATH);
+	
+	public static LocatorObject Contact_Submit_Button = new LocatorObject("//div[@class='form-group']/div[8]/a", "Submmit btton", LocatorObject.XPATH);
+	public static LocatorObject Contact_Message_Label = new LocatorObject("//div[@class='alert alert-success']", " message", LocatorObject.XPATH);
 	
 	/**
 	 * @param strSubject
@@ -34,27 +36,52 @@ public class ContactPage extends MasterPage {
 	 * @param strMessage
 	 * @return Verify the message can be send to the customer care including the negative tests for the fields 
 	 */
-	public ContactPage VerifyContactUs(String strSubject,String strEmail, String strOrder, String strMessage) {
-		Action.ActionLog("Expected Result: Verify the message can be send to customer service.");
-		Action.Click(Contact_Send_Button);
-		if(Action.GetText(Contact_Error_Label).contains("Invalid email address."))
-			Action.ActionLog("Blank or invalid email address verified successfully");
-		Action.enterText(Contact_Email_Textbox, strEmail);
-		Action.Click(Contact_Send_Button);
-		if(Action.GetText(Contact_Error_Label).contains("The message cannot be blank."))
-			Action.ActionLog("Black message verified successfully");
-		Action.enterText(Contact_Message_TextArea, strMessage);
-		Action.Click(Contact_Send_Button);
-		if(Action.GetText(Contact_Error_Label).contains("Please select a subject from the list provided."))
-			Action.ActionLog("Black Subject verified successfully");
-		Action.SelectdropdrownValueByVisibleText(Contact_Subject_Dropdown, "Customer service");
-		Action.Click(Contact_Send_Button);
-		Action.enterText(Contact_Order_Textbox, strOrder);
-		Action.Click(Contact_Send_Button);
-		if(Action.GetText(Contact_Success_Label).contains("Your message has been successfully sent to our team."))
-			Action.ActionLog("Actual Result: The query send o customer service successsfully");
+	public ContactPage VerifyFormSubmit(String First,String Last, String Job, String Education, String Sex, String Experience,String Date) {
+		Action.ActionLog("Expected Result: Verify the form is subnmitted succesfully");
+		Action.enterText(Contact_Firstname_Textbox, First);
+		Action.ActionLog("First name entered "+First);
+		Action.enterText(Contact_Lastname_Textbox, Last);
+		Action.ActionLog("Last name entered "+Last);
+		Action.enterText(Contact_Jobtitle_Textbox, Job);
+		Action.ActionLog("Job title entered "+Job);
+		switch(Education){
+		case "High School":
+			i=2;
+			break;
+		case "College":
+			i=3;
+			break;
+		case "Grad School":
+			i=4;
+			break;
+		}
+		LocatorObject Contact_Education_Radiobutton = new LocatorObject("//div[@class='form-group']/div[4]/div["+i+"]/input", "Contact error message", LocatorObject.XPATH);
+		Action.Click(Contact_Education_Radiobutton);
+		Action.ActionLog("Education selected "+Education);
+		switch(Sex){
+		case "Male":
+			i=2;
+			break;
+		case "Female":
+			i=3;
+			break;
+		case "Prefer not to say":
+			i=4;
+			break;
+		}
+		LocatorObject Contact_Sex_Check = new LocatorObject("//div[@class='form-group']/div[5]/div["+i+"]/input", "Contact Message",LocatorObject.XPATH);
+		Action.Click(Contact_Sex_Check);
+		Action.ActionLog("Sex selected "+Sex);
+		Action.SelectdropdrownValueByVisibleText(Contact_Yearofexp_Dropdown, Experience);
+		Action.ActionLog("Selected year of experience "+Experience);
+		Action.enterText(Contact_Date_Textbox, Date);
+		Action.ActionLog("Date enetered "+Date);
+		Action.Click(Contact_Submit_Button);
+		Action.ActionLog("Clicked on submit button");
+		if(Action.GetText(Contact_Message_Label).contains("The form was successfully submitted!"))
+			Action.ActionLog("Actual Result: The form submitted successfully");
 		else
-			Action.AssertFail("Actual Result: The message sent to customer service failed.");
+			Action.AssertFail("Actual Result: Form submission failed");
 		return this;
 	}
 }
